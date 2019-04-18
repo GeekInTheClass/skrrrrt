7866d8c5e6cff4e26266803dea83a2f538fc493f
2014004420 <2014004420@hanyang.ac.kr>
2019-04-18 16:31:15 +0900

    Initial Commit

---
+++ Iffy_singleview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Iffy_singleview
+//
+//  Created by YeongSoo Kim on 18/04/2019.
+//  Copyright © 2019 YeongSoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
